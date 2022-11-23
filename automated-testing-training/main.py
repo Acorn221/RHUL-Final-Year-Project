30b7df0e0510f113550cad5badb799536c321f85
James Arnott <jamesarnott2@gmail.com>
2022-11-23 21:28:27 +0000

    feat: got the model generator + logger to save the model and save the training results to a JSON file
    Former-commit-id: 2cc30e515683159fd60dee50e94a73f50b81779b

--- automated-testing-training/main.py
+++ automated-testing-training/main.py
@@ -9,6 +9,7 @@ import numpy as np
 from tensorflow import keras
 from pathlib import Path
 import os.path
+import json
 
 data_dir = Path('Alzheimers-classification/data/AugmentedAlzheimerDataset/');
 
@@ -35,16 +36,24 @@ params = {
 	"weights": 'imagenet'
 }
 
+def merge_dicts(dict1, dict2):
+	# Get the union of dict1 and dict2
+  keys = set(dict1).union(dict2)
+  arr = []
+  return dict((k, dict1.get(k, arr) + dict2.get(k, arr)) for k in keys)
+
 pretrainedUnfrozenLayers = 5
 
 models = [MobileNetV2, MobileNetV3Small, MobileNetV3Large, InceptionResNetV2, InceptionV3, ResNet101, VGG19]
+modelNames = ["MobileNetV2", "MobileNetV3Small", "MobileNetV3Large", "InceptionResNetV2", "InceptionV3", "ResNet101", "VGG19"]
+
 
 MobileNetV3Small = keras.applications.MobileNetV3Small(**params)
 
-for pretrainedModel in models:
+for i in range(len(models)):
 	# Create the model
 
-
+	pretrainedModel = models[i]
 
 	model = keras.Sequential()
 
@@ -67,30 +76,57 @@ for pretrainedModel in models:
 		loss='binary_crossentropy',
 	)
 
+	history = {}
 	# Train the model
-	history = model.fit(
+	for i in range(0, 20):
+		trainingStats = model.fit(
+			train_ds,
+			epochs=2, 
+			steps_per_epoch=len(train_ds),
+			validation_data=test_ds,
+		)
+		history = merge_dicts(history, trainingStats.history)
+
+		if(i > 5):
+			lastAccuracy = sum(history['accuracy'][:-5])
+			lastValAccuracy = sum(history['val_accuracy'][:-5])
+
+			if(lastAccuracy > (lastValAccuracy - 0.2)):
+				print("Likely Overfitting, moving to fine tuning now")
+				history['Overfitting excape'] = True
+				break;
+
+	# Unfreeze all the layers
+	for layer in model.layers:
+		layer.trainable = True
+
+	# Lower the training rate
+	model.compile(optimizer=keras.optimizers.Adam(0.000001),
+								loss=keras.losses.BinaryCrossentropy(),
+								metrics=['accuracy'])
+
+	# Train the model
+	trainingStats = model.fit(
 		train_ds,
-		epochs=2, 
+		epochs=40,
 		steps_per_epoch=len(train_ds),
 		validation_data=test_ds,
 	)
 
-	print("History:", history.history['accuracy'])
-	print(history.history['val_accuracy'])
-	print("History:", history.history)
-"""
-# Make all of the model layers trainable
-for layer in model.layers:
-	layer.trainable = True
+	# Add the training statistics to the array
+	history = merge_dicts(history, trainingStats.history)
 
-model.compile(optimizer=keras.optimizers.Adam(0.000001),
-              loss=keras.losses.BinaryCrossentropy(),
-              metrics=['accuracy'])
 
-# Train the model
-history = model.fit(
-  train_ds,
-  epochs=20,
-  steps_per_epoch=len(train_ds),
-  validation_data=test_ds,
-)"""
+	# Get the model name to save the data to
+
+	# Set the misc info for reference
+	history['params'] = params
+	history['pretrainedUnfrozenLayers'] = pretrainedUnfrozenLayers
+	history['BaseModelName'] = modelNames[i]
+ 	# Save the data to a JSON file
+	with open("./automated-testing-training/training-results/"+modelNames[i]+".json", "w+") as outfile:
+		json.dump(history, outfile)
+
+	# Save the model for future reference
+	model.save('./automated-testing-training/completed-models/'+modelNames[i])
+
