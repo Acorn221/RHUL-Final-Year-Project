d6a62f4d2436ba738c473fab2f55bd1349505ea6
James Arnott <jamesarnott2@gmail.com>
2022-11-29 12:10:11 +0000

    fix: Keras.Sequential reserves memory, should only be called when used
    Former-commit-id: 87ad3963b1c68ddd74578a55bb7f73264c5babb5

--- automated-testing-training/OwnModels.py
+++ automated-testing-training/OwnModels.py
@@ -42,9 +42,9 @@ test_ds = train_datagen.flow_from_directory(
     subset='validation')
 
 params = {
-	"input_shape": (224,224,3),
-	"include_top": False,
-	"weights": 'imagenet'
+  "input_shape": (224,224,3),
+  "include_top": False,
+  "weights": 'imagenet'
 }
 
 outDir = "./automated-testing-training/BasicModels/"
@@ -55,115 +55,116 @@ history = {}
 
 
 def save_history():
-	global history
-	global modelName
-	global model
-	with open(outDir+modelName+".json", "w+") as fp:
-		json.dump(history, fp)
+  global history
+  global modelName
+  global model
+  with open(outDir+modelName+".json", "w+") as fp:
+    json.dump(history, fp)
 
-	model.save(outDir+modelName+".h5")
+  model.save(outDir+modelName+".h5")
 
 
-atexit.register(save_history)
+#atexit.register(save_history)
 def merge_dicts(dict1, dict2):
-	# Get the union of dict1 and dict2
+  # Get the union of dict1 and dict2
   keys = set(dict1).union(dict2)
   arr = []
   return dict((k, dict1.get(k, arr) + dict2.get(k, arr)) for k in keys)
 
 
 # Making my own models from scratch as a point of comparison
-model1 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  MaxPooling2D(2, 2),
-  Conv2D(64, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-  Flatten(),
-  Dense(128, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
+def getModel(model):
+  if(model == 1):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      MaxPooling2D(2, 2),
+      Conv2D(64, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Flatten(),
+      Dense(128, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
+  elif(model == 2):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      MaxPooling2D(2, 2),
+      Conv2D(128, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Conv2D(128, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Flatten(),
+      Dense(128, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
+  elif(model == 3):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      MaxPooling2D(2, 2),
+      Conv2D(512, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Conv2D(512, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Conv2D(512, 3, activation='relu'),
+      MaxPooling2D(2, 2),
+      Flatten(),
+      Dense(128, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
+  elif(model == 4):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      Flatten(),
+      Dense(512, activation='relu'),
+      Dense(256, activation='relu'),
+      Dense(512, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
+  elif(model == 5):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      Flatten(),
+      Dense(1024, activation='relu'),
+      Dense(512, activation='relu'),
+      Dense(1024, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
+  elif(model == 6):
+    return keras.Sequential([
+      Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+      Flatten(),
+      Dense(1024, activation='relu'),
+      Dense(512, activation='relu'),
+      Dense(1024, activation='relu'),
+      Dense(4, activation='sigmoid')
+    ])
 
-model2 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  MaxPooling2D(2, 2),
-  Conv2D(128, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-	Conv2D(128, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-  Flatten(),
-  Dense(128, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
 
-model3 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  MaxPooling2D(2, 2),
-  Conv2D(512, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-	Conv2D(512, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-	Conv2D(512, 3, activation='relu'),
-  MaxPooling2D(2, 2),
-  Flatten(),
-  Dense(128, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
+for i in range(1, 6):
+  miscInfo = {}
+  modelName = 'Model'+str(i)
+  model = getModel(i)
+  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.001), metrics = ['accuracy'])
+  for j in range(0, 8):
+    trainingStats = model.fit(train_ds,
+      validation_data=test_ds,
+      epochs=6)
 
-model4 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  Flatten(),
-  Dense(512, activation='relu'),
-	Dense(256, activation='relu'),
-	Dense(512, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
+    history = merge_dicts(history, trainingStats.history)	
 
-model5 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  Flatten(),
-  Dense(1024, activation='relu'),
-	Dense(512, activation='relu'),
-	Dense(1024, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
+  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.0001), metrics = ['accuracy'])
+  
+  for j in range(20):
+    trainingStats = model.fit(train_ds,
+      validation_data=test_ds,
+      epochs=6)
+    
+    history = merge_dicts(history, trainingStats.history)
 
-model6 = keras.Sequential([
-  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
-  Flatten(),
-  Dense(1024, activation='relu'),
-	Dense(512, activation='relu'),
-	Dense(1024, activation='relu'),
-  Dense(4, activation='sigmoid')
-])
+  miscInfo['BaseModelName'] = modelName
+  saveInfo = {**history, **miscInfo}
 
-models = [model1, model2, model3, model4, model5, model6]
-modelNames = ["model1", "model2", "model3", "model4", "model5", "model6"]
+  with open("./automated-testing-training/BasicModels/training-results/"+modelName+".json", "w+") as outfile:
+    json.dump(saveInfo, outfile)
 
-for i in range(len(models)):
-	miscInfo = {}
-	modelName = modelNames[i]
-	model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.001), metrics = ['accuracy'])
-	for j in range(0, 8):
-		trainingStats = model.fit(train_ds,
-			validation_data=test_ds,
-			epochs=6)
-
-		history = merge_dicts(history, trainingStats.history)	
-
-	model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.0001), metrics = ['accuracy'])
-	
-	for j in range(20):
-		trainingStats = model.fit(train_ds,
-			validation_data=test_ds,
-			epochs=6)
-		
-		history = merge_dicts(history, trainingStats.history)
-
-	miscInfo['BaseModelName'] = modelNames[i]
-	saveInfo = {**history, **miscInfo}
-
-	with open("./automated-testing-training/BasicModels/training-results/"+modelNames[i]+".json", "w+") as outfile:
-		json.dump(saveInfo, outfile)
-
-	model.save('./automated-testing-training/BasicModels/completed-models/'+modelName)
+  model.save('./automated-testing-training/BasicModels/completed-models/'+modelName)
 
