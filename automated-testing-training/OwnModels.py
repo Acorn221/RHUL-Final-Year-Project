878dc424c1cf2744f0a407533a65de6ce955a9df
James Arnott <jamesarnott2@gmail.com>
2022-11-29 22:16:24 +0000

    fix: releases ram when completed training model, should help prevent crashes
    Former-commit-id: d8175e33e9cf7455ea03b822b496858f5eca4e71

--- automated-testing-training/OwnModels.py
+++ automated-testing-training/OwnModels.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
+from tensorflow.keras.callbacks import Callback
+from tensorflow.keras import backend as k
 import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
@@ -54,6 +56,8 @@ model = keras.Sequential()
 history = {}
 
 
+
+
 def save_history():
   global history
   global modelName
@@ -64,12 +68,18 @@ def save_history():
   model.save(outDir+modelName+".h5")
 
 
-#atexit.register(save_history)
+atexit.register(save_history)
 def merge_dicts(dict1, dict2):
   # Get the union of dict1 and dict2
   keys = set(dict1).union(dict2)
   arr = []
   return dict((k, dict1.get(k, arr) + dict2.get(k, arr)) for k in keys)
+
+# Credits to https://stackoverflow.com/a/67138072/5758415
+class ClearMemory(Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        gc.collect()
+        k.clear_session()
 
 
 # Making my own models from scratch as a point of comparison
@@ -139,24 +149,24 @@ def getModel(model):
     ])
 
 
-for i in range(1, 6):
+for i in range(4, 6):
   miscInfo = {}
   modelName = 'Model'+str(i)
   model = getModel(i)
-  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.001), metrics = ['accuracy'])
+  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.001), metrics = ['accuracy'], run_eagerly=True)
   for j in range(0, 8):
     trainingStats = model.fit(train_ds,
       validation_data=test_ds,
-      epochs=6)
+      epochs=6, callbacks=ClearMemory())
 
     history = merge_dicts(history, trainingStats.history)	
 
-  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.0001), metrics = ['accuracy'])
+  model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.0001), metrics = ['accuracy'], run_eagerly=True)
   
   for j in range(20):
     trainingStats = model.fit(train_ds,
       validation_data=test_ds,
-      epochs=6)
+      epochs=6, callbacks=ClearMemory())
     
     history = merge_dicts(history, trainingStats.history)
 
