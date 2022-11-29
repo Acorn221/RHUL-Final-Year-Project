57cf7dde777f96c53b58869458d419c29a15c23c
James Arnott <jamesarnott2@gmail.com>
2022-11-29 11:52:35 +0000

    feat: created a testing file with a list of non-transfer learned models as a point of reference
    Former-commit-id: 3196765f8e0b7e28894e1f1ef5105d9a9ee31acc

---
+++ automated-testing-training/OwnModels.py
@@ -0,0 +1,169 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
+import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+from pathlib import Path
+import os.path
+import json
+import atexit
+
+data_dir = Path('Alzheimers-classification/data/OriginalDataset');
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+    validation_split=0.2,
+    featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=360,
+    width_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    vertical_flip=True,
+) # set validation split
+
+train_ds = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=32,
+
+    class_mode='categorical',
+    subset='training')
+
+test_ds = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation')
+
+params = {
+	"input_shape": (224,224,3),
+	"include_top": False,
+	"weights": 'imagenet'
+}
+
+outDir = "./automated-testing-training/BasicModels/"
+
+modelName = ''
+model = keras.Sequential()
+history = {}
+
+
+def save_history():
+	global history
+	global modelName
+	global model
+	with open(outDir+modelName+".json", "w+") as fp:
+		json.dump(history, fp)
+
+	model.save(outDir+modelName+".h5")
+
+
+atexit.register(save_history)
+def merge_dicts(dict1, dict2):
+	# Get the union of dict1 and dict2
+  keys = set(dict1).union(dict2)
+  arr = []
+  return dict((k, dict1.get(k, arr) + dict2.get(k, arr)) for k in keys)
+
+
+# Making my own models from scratch as a point of comparison
+model1 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  MaxPooling2D(2, 2),
+  Conv2D(64, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+  Flatten(),
+  Dense(128, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+model2 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  MaxPooling2D(2, 2),
+  Conv2D(128, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+	Conv2D(128, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+  Flatten(),
+  Dense(128, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+model3 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  MaxPooling2D(2, 2),
+  Conv2D(512, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+	Conv2D(512, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+	Conv2D(512, 3, activation='relu'),
+  MaxPooling2D(2, 2),
+  Flatten(),
+  Dense(128, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+model4 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  Flatten(),
+  Dense(512, activation='relu'),
+	Dense(256, activation='relu'),
+	Dense(512, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+model5 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  Flatten(),
+  Dense(1024, activation='relu'),
+	Dense(512, activation='relu'),
+	Dense(1024, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+model6 = keras.Sequential([
+  Conv2D(32, 3, activation='relu', input_shape=(224, 224, 3)),
+  Flatten(),
+  Dense(1024, activation='relu'),
+	Dense(512, activation='relu'),
+	Dense(1024, activation='relu'),
+  Dense(4, activation='sigmoid')
+])
+
+models = [model1, model2, model3, model4, model5, model6]
+modelNames = ["model1", "model2", "model3", "model4", "model5", "model6"]
+
+for i in range(len(models)):
+	miscInfo = {}
+	modelName = modelNames[i]
+	model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.001), metrics = ['accuracy'])
+	for j in range(0, 8):
+		trainingStats = model.fit(train_ds,
+			validation_data=test_ds,
+			epochs=6)
+
+		history = merge_dicts(history, trainingStats.history)	
+
+	model.compile(loss = 'categorical_crossentropy', optimizer = tf.keras.optimizers.Adam(0.0001), metrics = ['accuracy'])
+	
+	for j in range(20):
+		trainingStats = model.fit(train_ds,
+			validation_data=test_ds,
+			epochs=6)
+		
+		history = merge_dicts(history, trainingStats.history)
+
+	miscInfo['BaseModelName'] = modelNames[i]
+	saveInfo = {**history, **miscInfo}
+
+	with open("./automated-testing-training/BasicModels/training-results/"+modelNames[i]+".json", "w+") as outfile:
+		json.dump(saveInfo, outfile)
+
+	model.save('./automated-testing-training/BasicModels/completed-models/'+modelName)
+
