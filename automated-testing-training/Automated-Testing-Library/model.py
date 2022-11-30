b5471fbc0d4ce5a5b47900b54f3bc7f19fe9800e
James <jamesarnott2@gmail.com>
2022-11-30 14:23:23 +0000

    fix: saving more information, added compile model function
    Former-commit-id: bcf2efd57caf6a00baca83eba2dd71e1f450c78d

--- automated-testing-training/Automated-Testing-Library/model.py
+++ automated-testing-training/Automated-Testing-Library/model.py
@@ -4,24 +4,31 @@ import time
 # The directory where the history will be saved along with the models
 modelDir = 'TrainedModels/'
 
+# TODO: clear memory after each model is trained
+# TODO: make sure everything is saved to the correct directory
+# TODO: allow quick loading of models for quick testing/training
+# TODO: allow quick and easy statistics to be generated
+# TODO: allow for models to be easily trained after a crash or interruption
+
 class Model:
 	def __init__(self, modelCallback, modelname):
-			self.modelCallback = modelCallback
-			self.sequentialModel = None
-			self.modelName = modelname
-			self.history = None
-			self.trained = False
-			self.epochs = 0
-			self.trainingTime = 0
+		self.modelCallback = modelCallback
+		self.sequentialModel = None
+		self.modelName = modelname
+		self.history = None
+		self.trained = False
+		self.epochs = 0
+		self.trainingTime = 0
 
-	def loadModel(self, sequentialModel):
-			self.sequentialModel = self.modelCallback()
+	def loadModel(self):
+		self.sequentialModel = self.modelCallback()
 
 	def saveHistory(self):
-			with open(modelDir+self.modelName+".json", "w+") as fp:
-					json.dump(self.history, fp)
+		infoToSave = { 'history': self.history.history, 'epochs': self.epochs, 'trainingTime': self.trainingTime }
+		with open(modelDir+self.modelName+".json", "w+") as fp:
+				json.dump(infoToSave, fp)
 
-			self.sequentialModel.save(modelDir+self.modelName+".h5")
+		self.sequentialModel.save(modelDir+self.modelName+".h5")
 
 	def mergeHistory(self, history):
 		# Get the union of dict1 and dict2
@@ -29,8 +36,10 @@ class Model:
 		arr = []
 		self.history = dict((k, self.history.get(k, arr) + history.get(k, arr)) for k in keys)
 
+	def compileModel(self, optimizer, loss, metrics):
+		self.sequentialModel.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
 	def train(self, train_ds, test_ds, epochs):
-		# Record the time it takes to train the model
 		startTime = time.time()
 		history = self.sequentialModel.fit(train_ds, epochs=epochs, validation_data=test_ds)
 		self.mergeHistory(history.history)
