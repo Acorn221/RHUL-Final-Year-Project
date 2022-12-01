a2a978250c8638d8b7b572d14a020b061ea584d2
James <jamesarnott2@gmail.com>
2022-12-01 00:29:38 +0000

    fix: tests now run for models
    Former-commit-id: dcb2b415154f82632a8909f366454591a9b61428

--- AutomatedTestingLibrary/Tests/test_model.py
+++ AutomatedTestingLibrary/Tests/test_model.py
@@ -4,16 +4,26 @@ from utils import MobileNetModel
 
 model = m.Model(MobileNetModel, "MobileNetV2")
 
+automatedTesting = t.AutomatedTesting([model], "Alzheimers-classification/data", "output")
+
 def test_load():
 	# Load the model
 	model.loadModel()
 	# Check if the model is loaded
-	assert model.model is not None
+	assert model.sequentialModel is not None
 
 def test_train():
 	# Load the model
 	model.loadModel()
+
+	# compile the model
+	model.compileModel(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+	# load the training data
+	automatedTesting.loadTrainingData()
+
 	# Train the model
-	model.trainModel()
+	model.train(automatedTesting.train_ds, automatedTesting.test_ds, 1)
+
 	# Check if the model is trained
-	assert model.model.predict()
+	assert model.model.predict(automatedTesting.test_ds.next()[0][0].shape) is not None
