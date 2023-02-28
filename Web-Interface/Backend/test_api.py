7071e3732e82e802e1453541db7f874e249e7703
James <jamesarnott2@gmail.com>
2023-02-28 13:50:44 +0000

    test: added test upload of a file that is not an image
    Former-commit-id: cab4091cac7cf1b62153e8ee27f34cad69426327

--- Web-Interface/Backend/test_api.py
+++ Web-Interface/Backend/test_api.py
@@ -2,15 +2,28 @@
 # Import the Flask testing library
 from Web-Interface.Backend.main import app
 
+# This is needed as the pytest command should be ran from the root of the project
+relativePath = "Web-Interface/Backend/Test_Files/"
 
+"""
+This test is to check if the API responds with a 200 status code when a valid image is uploaded
+"""
 def test_endpoint():
 	# Create a test client using the Flask application configured for testing
 	with app.test_client() as client:
 			# Send a GET request to the API endpoint
-			response = client.post('/predict', data=dict(file=(open('test_image.jpg', 'rb'), 'test_image.jpg')))
+			response = client.post('/predict', data=dict(file=(open(relativePath+'Target.png', 'rb'), 'test_image.jpg')))
 			# Check that the response is valid
 			assert response.status_code == 200
 			# Check that the response is a JSON object
 			assert response.is_json
 			# Check that the JSON object contains the key 'prediction'
 			assert 'prediction' in response.get_json()
+
+def test_not_image():
+	# Create a test client using the Flask application configured for testing
+	with app.test_client() as client:
+			# Send a GET request to the API endpoint
+			response = client.post('/predict', data=dict(file=(open(relativePath+'Not_An_Image.exe', 'rb'), 'Not_An_Image.exe')))
+			# Check that the response is valid
+			assert response.status_code == 400
