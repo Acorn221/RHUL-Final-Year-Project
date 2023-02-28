d11fa814a343db2bf187170f1c51425fae7f0cd8
James <jamesarnott2@gmail.com>
2023-02-28 11:52:02 +0000

    feat: added states logic for uploading, added post request for image
    Former-commit-id: cec3bed2e9135c3b3ba118f94339b84effa67378

--- Web-Interface/src/App.tsx
+++ Web-Interface/src/App.tsx
@@ -1,17 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import '@/index.css';
 
+enum states {
+  waitingForFile,
+  waitingForPrediction,
+  predictionReceived,
+}
+
 const App = () => {
-  const [image, setImage] = useState({});
+  const [currentState, setCurrentState] = useState<states>(states.waitingForFile);
+  // blob or undefined
+  const [image, setImage] = useState<Blob | undefined>(undefined);
+
+  const imageUploaded = (event) => {
+    setImage(event.target.files[0]);
+    setCurrentState(states.waitingForPrediction);
+  };
+
+  useEffect(() => {
+    if (currentState === states.waitingForPrediction) {
+      const formData = new FormData();
+      if (image === undefined) return;
+      formData.append('file', image);
+      fetch('http://localhost:5000/predict', {
+        method: 'POST',
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setCurrentState(states.predictionReceived);
+        });
+    }
+  }, [currentState]);
 
   return (
     <div className="flex justify-center align-middle h-screen">
       <div className="bg-white m-auto p-10 rounded-xl w-3/4 md:w-1/2 text-center">
         <div className="underline text-5xl">Alzheimer&apos;s Disease Predictor</div>
         <div className="m-5 text-left">
-          <div className="text-2xl p-20 text-center bg-slate-300 hover:bg-slate-400 rounded-2xl">
-            Drag and drop your MRI image here, or click to select the file
+          <div className="text-2xl p-20 text-center bg-slate-300 hover:bg-slate-400 rounded-2xl select-none">
+            {states.waitingForFile === currentState && (
+            <div>
+              <input type="file" name="file" onChange={imageUploaded} />
+              <br />
+              Drag and drop your MRI image here, or click to select the file
+            </div>
+            )}
           </div>
         </div>
       </div>
