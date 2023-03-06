21a5772e61699d33d2ff3d770f2044ba681571d9
James <jamesarnott2@gmail.com>
2023-03-06 21:50:47 +0000

    feat: added small script to rename all the files and convert them from gifs to PNGs
    Former-commit-id: a6d078cb046b4ebd6f306facfc77ce7c1bfc5e69

---
+++ OASIS/rename.py
@@ -0,0 +1,31 @@
+import os
+
+path = "C:\Active-Projects\RHUL-FYP\PROJECT\OASIS\processed_scans"
+
+files = os.listdir(path)
+
+for file in files:
+  # Rename the file to clip the last 30 characters
+  full_path = os.path.join(path, file)
+
+  new_name = file[:-42] + ".gif"
+  
+  # Rename the file
+  os.rename(full_path, os.path.join(path, new_name))
+
+
+# Convert the .gif files to .png
+from PIL import Image
+
+# Loop through all files in the directory
+for filename in os.listdir(path):
+    if filename.endswith(".gif"):
+        # Open the .gif file
+        gif_file = Image.open(os.path.join(path, filename))
+
+        # Convert to .png format
+        png_file = gif_file.convert('RGB')
+
+        # Save the new .png file
+        png_filename = os.path.splitext(filename)[0] + ".png"
+        png_file.save(os.path.join(path, png_filename))
