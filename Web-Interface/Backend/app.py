ab51fea69eb4a43af9c682fc841ac0b6689f5e22
James <jamesarnott2@gmail.com>
2023-03-16 23:49:54 +0000

    fix: the api endpoint now works with the frontend also deletes files after they have been processed
    Former-commit-id: 9016d41a85f139176d11141552d518b670e785fb

--- Web-Interface/Backend/app.py
+++ Web-Interface/Backend/app.py
@@ -5,11 +5,14 @@ from werkzeug.utils import secure_filename
 # Import keras and other libraries for processing the MRI image
 from keras.models import load_model
 from keras.preprocessing import image
+from os import path, remove
 import numpy as np
 import tensorflow as tf
 
 # Define the flask app
 app = Flask(__name__)
+
+relativePath = path.dirname(path.abspath(__file__))
 
 # Load the model TODO: add model to test with 
 #model = load_model('model.h5')
@@ -29,21 +32,25 @@ def upload():
 	# Get the file from the request
 	uploadedFile = request.files['file']
 	# Save the file to ./uploads
-	filepath = './uploads/' + secure_filename(uploadedFile.filename)
+	filepath = relativePath + '/uploads/' + secure_filename(uploadedFile.filename)
 	uploadedFile.save(filepath)
 
 	age = request.form['age']
 	sex = request.form['sex']
 	mmse = request.form['mmse']
-	
+
 	# Make prediction TODO: make prediction
 	# prediction = predict_class(filepath, model)
 	prediction = {'0': '0.0', '1': '0.82', '2': '0.18', '3': '0.01'}
 	# Convert the response to a string
 	response = str(prediction)
+
+	# Delete the file from the server
+	remove(filepath)
+
 	return response
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=3001)
 	
