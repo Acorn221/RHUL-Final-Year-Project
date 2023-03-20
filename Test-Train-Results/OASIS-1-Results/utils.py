bebb56dfe8019d00cb910cbbedde8127b4852be0
James <jamesarnott2@gmail.com>
2023-03-20 20:48:31 +0000

    chore: created utils file
    Former-commit-id: 2e376eb1d9050eebc835e8216e729bbab1819fc8

---
+++ Test-Train-Results/OASIS-1-Results/utils.py
@@ -0,0 +1,22 @@
+import os
+import math
+import numpy as np
+
+# The directory where the processed scans are stored
+directory='C:/Active-Projects/RHUL-FYP/PROJECT/OASIS/1/processed_scans/'
+
+
+def createFileName(id):
+	return directory + id + ".png"
+
+def convertToString(cdr):
+	if str(cdr) == 'nan':
+		return '0'
+	else:
+		return str(cdr)
+
+def mmseConvert(x):
+	if math.isnan(float(x)):
+		return np.random.randint(25, 30)
+	else:
+		return x
