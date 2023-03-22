d6dc829fffc46bd91b5cf4796d736e18a6368a18
James <jamesarnott2@gmail.com>
2023-03-22 13:34:42 +0000

    feat: created one hot encoding function for CDR
    Former-commit-id: d858c65c5e8f311ca051437d7a8143fcb7dc8582

--- Test-Train-Results/OASIS-1-Results/utils.py
+++ Test-Train-Results/OASIS-1-Results/utils.py
@@ -23,3 +23,15 @@ def mmseConvert(x):
 
 def genderToFloat(x):
 	return 1 if x == 'M' else 0
+
+def cdr_formatting(s):
+
+	values = ['0.0', '0.5', '1.0', '2.0']
+
+	one_hot_array = np.zeros(len(values), dtype=int)
+
+	index = values.index(str(float(s)))
+
+	one_hot_array[index] = 1
+
+	return one_hot_array
