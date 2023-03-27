3576a221391044d6dfaab327e016a0217c362fda
James <jamesarnott2@gmail.com>
2023-03-27 22:41:21 +0100

    refactor: fixing errors
    Former-commit-id: d60149cd5e0ea9183d18f4cefd3f152b6c5b7df2 [formerly 9becc9ffede1e27443921fabc32e5c375edc09f4]
    Former-commit-id: 5e017fde2945096edf392913f36b21be969d190c

--- Web-Interface/Frontend/src/misc/HomePage.tsx
+++ Web-Interface/Frontend/src/misc/HomePage.tsx
@@ -7,11 +7,11 @@ const HomePage = () => (
   <div className="flex flex-col items-center justify-center h-screen">
     <div className="flex flex-col items-center justify-center m-3 max-w-4xl bg-white p-5 rounded-2xl">
       <div className="text-4xl font-bold">Welcome to the Disease Predicter</div>
-      <div className="text-2xl">This is a web application that predicts the probability of Alzheimer's disease, or predicts whether or not a skin blemish is benign or malignant</div>
+      <div className="text-2xl">This is a web application that predicts the probability of Alzheimer&apos;s disease, or predicts whether or not a skin blemish is benign or malignant</div>
       <div className="text-2xl">To get started, click on the button below.</div>
       <div className="w-full justify-center flex flex-auto m-2 gap-2">
         <Link to="/AD-Predicter">
-          <Button variant="contained" size="large" className="max-w-4xl h-20">Alzheimer's Predicter</Button>
+          <Button variant="contained" size="large" className="max-w-4xl h-20">Alzheimer&apos;s Predicter</Button>
         </Link>
         <Link to="/Skin-Cancer-Predicter">
           <Button variant="contained" size="large" className="max-w-4xl h-20">Skin Cancer Predicter</Button>
